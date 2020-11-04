4f1ac89208faa502f4e66c68833f6c7e7a743365
Karn Tirasoontorn <karn@hey.com>
2020-11-04 10:42:23 +0700

    Test: actions cache

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -8,12 +8,28 @@ RSpec.describe 'Calculator' do
       # Arrange
       a = 2
       b = 2
+      expected_result = 4
 
       # Act
       result = calculator.add(a, b)
 
       # Assert
-      expect(result).to eq(4)
+      expect(result).to eq(expected_result)
+    end
+  end
+
+  context "Minus" do
+    it '2 - 1 should be 1' do
+      # Arrange
+      a = 2
+      b = 1
+      expected_result = 1
+
+      # Act
+      result = calculator.minus(a, b)
+
+      # Assert
+      expect(result).to eq(expected_result)
     end
   end
 end
